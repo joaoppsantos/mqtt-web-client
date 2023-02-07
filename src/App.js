ba7415d78ecfa074834704f545ae23b4d58af2a4
joaoppsantos <joaoppsantos@live.com>
2023-02-07 17:30:47 +0000

    feat: Add topic subscribe feature

--- src/App.js
+++ src/App.js
@@ -10,9 +10,50 @@ function MqttClient() {
   const [message, setMessage] = useState('');
   const [receivedMessage, setReceivedMessage] = useState('');
   const [receivedTopic, setReceivedTopic] = useState('');
+  const [topicCandidate, setTopicCandidate] = useState('');
+  const [subscribedTopic, setSubscribedTopic] = useState([]);
   const [clientId] = useState(`${Math.random().toString(16).substr(2, 8)}`);
   let client;
 
+  const shouldSubscribe = (topic) => {
+    if (subscribedTopic.indexOf(topic) === -1) {
+      setSubscribedTopic([...subscribedTopic, topic]);
+    }
+  };
+  const handleSubscribe = (event) => {
+    event.preventDefault();
+
+    if (!client || !client.connected) {
+      client = mqtt.connect({
+        clientId: clientId,
+        protocol: 'wss',
+        hostname: hostname,
+        protocolVersion: 4,
+        port: 8884,
+        path: '/mqtt',
+        clean: true,
+        resubscribe: true,
+        keepalive: 60,
+        reconnectPeriod: 0,
+        username: username,
+        password: password,
+      });
+      client.on('connect', () => {
+        client.subscribe(topicCandidate, (error) => {
+          if (!error) {
+            console.log('Connected');
+          }
+        });
+        client.on('message', (topicCandidate, clientMessage) => {
+          if (message !== clientMessage.toString()) {
+            setReceivedMessage(clientMessage.toString());
+            setReceivedTopic(topicCandidate);
+          }
+        });
+      });
+    }
+    shouldSubscribe(topicCandidate);
+  };
   const handlePublish = (event) => {
     event.preventDefault();
     if (!client || !client.connected) {
@@ -109,6 +150,27 @@ function MqttClient() {
           </button>
         </div>
       </form>
+      <h3>Topics</h3>
+      <form>
+        <label>
+          SUBSCRIBE TO:
+          <input
+            className="label"
+            type="text"
+            value={topicCandidate}
+            onChange={(e) => setTopicCandidate(e.target.value)}
+          />
+        </label>
+        <br />
+        <div className="button">
+          <button onClick={(e) => handleSubscribe(e)}>Subscribe</button>
+        </div>
+      </form>
+      <ul>
+        {subscribedTopic.map((candidate, index) => (
+          <li key={index}>{candidate}</li>
+        ))}
+      </ul>
       <p>
         RECEIVED MESSAGE: {receivedMessage}{' '}
         {receivedTopic && <span>FROM {receivedTopic}</span>}
